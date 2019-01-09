fdd257212c3afcb36e21883337af6cd7d2722a26
Russ Ferriday <russ.ferriday@gmail.com>
2019-01-09 12:12:28 +0000

    failing config for django-storages

--- config/settings/zappadev.py
+++ config/settings/zappadev.py
@@ -93,3 +93,55 @@ DATABASES = {
 }
 
 DATABASES['default']['ATOMIC_REQUESTS'] = True
+
+
+# STORAGES
+# ------------------------------------------------------------------------------
+# https://django-storages.readthedocs.io/en/latest/#installation
+INSTALLED_APPS += ['storages']  # noqa F405
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+# zappa-hari-umfrage-s3-access
+AWS_ACCESS_KEY_ID = env('ZAPPA_S3_AWS_ACCESS_KEY_ID')
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_SECRET_ACCESS_KEY = env('ZAPPA_S3_AWS_SECRET_ACCESS_KEY')
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_STORAGE_BUCKET_NAME = env('ZAPPA_S3_AWS_STORAGE_BUCKET_NAME')
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_QUERYSTRING_AUTH = False
+# DO NOT change these unless you know what you're doing.
+_AWS_EXPIRY = 60 * 60 * 24 * 7
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': f'max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate',
+}
+AWS_DEFAULT_ACL = env('ZAPPA_S3_AWS_DEFAULT_ACL')
+AWS_BUCKET_ACL = env('ZAPPA_S3_AWS_BUCKET_ACL')
+AWS_AUTO_CREATE_BUCKET = True
+
+# STATIC
+# ------------------------
+
+STATICFILES_STORAGE = 'config.settings.zappadev.StaticRootS3Boto3Storage'
+STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/'
+
+# MEDIA
+# ------------------------------------------------------------------------------
+
+# region http://stackoverflow.com/questions/10390244/
+# Full-fledge class: https://stackoverflow.com/a/18046120/104731
+from storages.backends.s3boto3 import S3Boto3Storage  # noqa E402
+
+
+class StaticRootS3Boto3Storage(S3Boto3Storage):
+    location = 'static'
+
+
+class MediaRootS3Boto3Storage(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = False
+
+
+# endregion
+DEFAULT_FILE_STORAGE = 'config.settings.production.MediaRootS3Boto3Storage'
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/'
+
