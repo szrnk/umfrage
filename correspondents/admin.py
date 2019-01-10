10a494bf676072351a635a54ac03687e0b55a2d6
Russ Ferriday <russ.ferriday@gmail.com>
2019-01-10 18:47:51 +0000

    Add inline Department under Hospital

--- correspondents/admin.py
+++ correspondents/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 from .models import Hospital, Department
 
-admin.site.register(Hospital)
+
+class DepartmentInline(admin.StackedInline):
+    model = Department
+
+
+class HospitalAdmin(admin.ModelAdmin):
+    inlines = [
+        DepartmentInline,
+    ]
+    list_display = ('name', 'city', 'state_province')
+
+
+admin.site.register(Hospital, HospitalAdmin)
 admin.site.register(Department)
