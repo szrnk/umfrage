daa01914b1eda0ab6842947014dec41740c4af87
Russ Ferriday <russ.ferriday@gmail.com>
2019-01-10 19:56:51 +0000

    Grouped into fieldsets, trimmed fields, added phone

--- correspondents/admin.py
+++ correspondents/admin.py
@@ -5,6 +5,15 @@ from .models import Hospital, Department
 
 class DepartmentInline(admin.StackedInline):
     model = Department
+    fieldsets = (
+        (None, {
+            'fields': (('name',),  ('contact_name', 'contact_phone', 'contact_email'), ('address', 'address_detail'),
+                       ('city', 'state_province', 'country'),
+                       ('website',),
+                       )
+        }),
+    )
+    extra = 1
 
 
 class HospitalAdmin(admin.ModelAdmin):
@@ -12,7 +21,29 @@ class HospitalAdmin(admin.ModelAdmin):
         DepartmentInline,
     ]
     list_display = ('name', 'city', 'state_province')
+    fieldsets = (
+        (None, {
+            'fields': (('name', 'phone', 'email'), ('address', 'address_detail'), ('city', 'state_province', 'country'), ('website',))
+        }),
+        # ('Advanced options', {
+        #     'classes': ('collapse',),
+        #     'fields': ('registration_required', 'template_name'),
+        # }),
+    )
+    extra = 1
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'hospital', 'city', 'state_province')
+    fieldsets = (
+        (None, {
+            'fields': (('name', 'hospital'), ('contact_name', 'contact_phone', 'contact_email'), ('address', 'address_detail'),
+                       ('city', 'state_province', 'country'),
+                       ('website',),
+                       )
+        }),
+    )
 
 
 admin.site.register(Hospital, HospitalAdmin)
-admin.site.register(Department)
+admin.site.register(Department, DepartmentAdmin)
--- correspondents/models.py
+++ correspondents/models.py
@@ -1,15 +1,20 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
+phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                             message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 
 
 class Hospital(models.Model):
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
-    address_detail = models.CharField(max_length=50, blank=True)
-    city = models.CharField(max_length=60)
-    state_province = models.CharField(max_length=30)
-    country = models.CharField(max_length=50)
-    website = models.URLField()
-    email = models.EmailField()
+    name = models.CharField(max_length=40)
+    address = models.CharField(max_length=30)
+    address_detail = models.CharField(max_length=40, blank=True)
+    city = models.CharField(max_length=30)
+    state_province = models.CharField(max_length=25)
+    country = models.CharField(max_length=45)
+    website = models.URLField(blank=True)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
 
     class Meta:
         ordering = ["name"]
@@ -20,15 +25,16 @@ class Hospital(models.Model):
 
 class Department(models.Model):
     hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
-    address_detail = models.CharField(max_length=50, blank=True)
-    city = models.CharField(max_length=60)
-    state_province = models.CharField(max_length=30)
-    country = models.CharField(max_length=50)
-    website = models.URLField()
-    contact_name = models.CharField(max_length=50)
-    contact_email = models.EmailField()
+    name = models.CharField(max_length=40)
+    address = models.CharField(max_length=30)
+    address_detail = models.CharField(max_length=40, blank=True)
+    city = models.CharField(max_length=30)
+    state_province = models.CharField(max_length=25)
+    country = models.CharField(max_length=45)
+    website = models.URLField(blank=True)
+    contact_name = models.CharField(max_length=40, blank=True)
+    contact_email = models.EmailField(blank=True)
+    contact_phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
 
     class Meta:
         ordering = ["name"]
