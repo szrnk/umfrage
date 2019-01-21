27ab79cee02945910581976af8750677e875be90
Russ Ferriday <russ.ferriday@gmail.com>
2019-01-21 11:30:31 +0000

    Fix sectionFactory order. Create tight structure.

--- surveys/tests/factories.py
+++ surveys/tests/factories.py
@@ -17,6 +17,7 @@ class SectionFactory(DjangoModelFactory):
 
     name = Sequence(lambda n: "Section %03d" % n)
     title = Faker('sentence', nb_words=7)
+    order = Sequence(lambda n: n)
 
     class Meta:
         model = Section
@@ -59,5 +60,26 @@ def basic_survey_structure(surveyname=None):
                 qu = QuestionFactory(section=se)
                 for opi in range(4):
                     _ = OptionFactory(question=qu)
+        surveys.append(su)
+    return surveys
+
+
+def tight_survey_structure(surveyname=None):
+    # allow a one-shot survey name
+    if surveyname is not None:
+        survey_kwargs = dict(name=surveyname)
+    else:
+        survey_kwargs = dict()
+
+    surveys = []
+    for sui in range(2):
+        su = SurveyFactory(**survey_kwargs)
+        survey_kwargs = dict()
+        for sei in range(4):
+            se = SectionFactory(survey=su, title=f'this is section {sei}')
+            for qui in range(4):
+                qu = QuestionFactory(section=se, text=f'text for question {qui}, with original order {qui}')
+                for opi in range(4):
+                    _ = OptionFactory(question=qu, text=f'text for option {opi}')
         surveys.append(su)
     return surveys
