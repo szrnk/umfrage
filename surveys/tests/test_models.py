6f47c6a0aebd0cd4fc69eefb283dccbcedbeb491
Russ Ferriday <russ.ferriday@gmail.com>
2019-01-28 15:21:44 +0000

    Fix Class error

--- surveys/tests/test_models.py
+++ surveys/tests/test_models.py
@@ -1,12 +1,12 @@
 import pytest
 from django.conf import settings
 
-from surveys.models import Question
+from surveys.models import Question, Option
 
 pytestmark = pytest.mark.django_db
 
 
-def test_option_parentage(option: Question):
+def test_option_parentage(option: Option):
     assert option.question is not None
     assert option.question.section is not None
     assert option.question.section.survey is not None
