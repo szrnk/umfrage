b7d78e6efeb4a889fcfd22571a22194798d93c57
Russ Ferriday <russ.ferriday@gmail.com>
2019-02-11 02:07:03 +0000

    Add create-pet-survey

---
+++ surveys/management/commands/create_pet_survey.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+
+from surveys.tests.factories import pet_survey
+from datetime import datetime
+
+
+class Command(BaseCommand):
+    help = "Create pet survey."
+
+    # def add_arguments(self, parser):
+    #     parser.add_argument('sample', nargs='+')
+
+    def handle(self, *args, **options):
+        pet_survey(f"Pet Survey {datetime.now()}")
+        self.stdout.write(self.style.SUCCESS("Done"))
