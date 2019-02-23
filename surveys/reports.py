1554eadf8e70646b2deab2a6988640943f853f39
Russ Ferriday <russ.ferriday@gmail.com>
2019-02-23 03:12:17 +0000

    First iteration of create_survey_output.

--- surveys/reports.py
+++ surveys/reports.py
@@ -1,11 +1,40 @@
-from surveys.models import Answer
+import tablib
+
+from surveys.models import Answer, OPTION_CHOICES
 
 
 def create_survey_output(survey):
-    aa = Answer.objects.filter(question__parent_section__survey_id=survey.id).order_by('department').all()
-    aa = list(aa)
-    for a in aa:
-        print(a.department, a.question.code, a.value, a.options.all(), a)
-    # for se in survey.sections():
-    #     for qu in se.questions():
-    #         pass
+    data = tablib.Dataset()
+    for se in survey.sections():
+        for qu in se.questions():
+            if qu.qtype in OPTION_CHOICES:
+                for op in qu.options().all():
+                    data.append([se.code, qu.code, op.code, op.text])
+            else:
+                data.append([se.code, qu.code, 'V', '-'])
+    data.headers = ['Section', 'Question', 'Op/Val', 'text']
+    for inv in survey.invitation_set.all():
+        col = []
+        for se in survey.sections():
+            for qu in se.questions():
+                ans = Answer.objects.filter(department=inv.department, question_id=qu.id).first()
+                if qu.qtype in OPTION_CHOICES:
+                    for op in qu.options().all():
+                        if se.triggered(inv.department):
+                            if qu.triggered(inv.department) and ans:
+                                o = ans.options.filter(id=op.pk).first()
+                                col.append('*' if o is not None else '')
+                            else:
+                                col.append('')
+                        else:
+                            col.append('')
+                elif ans is not None:
+                    col.append(ans.value.text)
+                else:
+                    col.append('')
+        data.append_col(col, header=inv.department.name)
+    with open('temp.xls', 'wb') as f:
+        f.write(data.xls)
+
+    # print(data.export('xls'))
+
